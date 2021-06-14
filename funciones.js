d650b5764778885b22bace4f495d214f1c1796c9
Facundo Frau <facufrau@gmail.com>
2021-06-14 20:02:07 -0300

    Crea funciones de resaltar botón y turno computadora
    Crea las funciones para resaltar uno de los botones y el turno de la computadora (seleccionar boton aleatorio y mostrar la secuencia)

--- funciones.js
+++ funciones.js
@@ -1,14 +1,37 @@
-function jugar() {
-    $circulos = document.querySelectorAll('.circulo');
-    let elementoBoton = seleccionarBotonAleatorio($circulos);
-    resaltarBoton(elementoBoton);
-}
-
 function seleccionarBotonAleatorio(listaBotones) {
-    const indice = Math.floor(Math.random() * (4 - 0)) + 0;
+    const indice = Math.floor(Math.random() * (4 - 0));
     return listaBotones[indice];
 }
 
-function resaltarBoton(elemento) {
+function resaltarElemento(elemento) {
+    elemento.style.opacity = 1.0;
+    setTimeout(function(){elemento.style.opacity = 0.5}, 800);
+}
+function compararJugadas(secuencia1, secuencia2) {
+    if (JSON.stringify(secuencia1) === JSON.stringify(secuencia2)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+function turnoComputadora(secuenciaComputadora) {
+    $circulos = document.querySelectorAll('.circulo');
+    let elementoBoton = seleccionarBotonAleatorio($circulos);
+    secuenciaComputadora.push(Number(elementoBoton.innerText) - 1);
+        
+    secuenciaComputadora.forEach((indiceElemento, contador) => {
+        let elementoAResaltar = $circulos[indiceElemento];
+        setTimeout(function(){resaltarElemento(elementoAResaltar)}, contador * 1000);
+    });
+}
 
+function turnoUsuario(secuenciaUsuario, secuenciaComputadora) {
+    $circulos = document.querySelectorAll('.circulo');
+    $circulos.forEach(elemento => {
+        elemento.addEventListener('click', function (event) 
+            {
+        secuenciaUsuario.push(elemento.innerText -  1);
+            });
+        })
 }
--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 let puntaje = 0;
 let puntajeMaximo = 0;
-let secuenciaGenerada = [];
+let secuenciaComputadora = [];
 let secuenciaUsuario = [];
+let activo = false;
 
 const $botonEmpezar = document.querySelector('#empezar');
-$botonEmpezar.onclick = jugar;
+$botonEmpezar.onclick = comenzarJuego;
+
+function jugarRonda(continuaRonda) {
+    if (continuaRonda) {
+        turnoComputadora(secuenciaComputadora);
+        turnoUsuario(secuenciaUsuario, secuenciaComputadora);
+    }
+    else {
+        console.log('Juego finalizado');
+    }
+
+    
+}
+
+function comenzarJuego() {
+    let continuaRonda = true;
+    for (let i = 1; i <= 6; i++){
+        if (i === 6) {
+            continuaRonda = false;
+        }
+        jugarRonda(continuaRonda);
+    }
+    
+}
