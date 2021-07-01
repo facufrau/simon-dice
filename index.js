37a105f549e37baec58644b0b86008c1aa44d2a2
Facundo Frau <facufrau@gmail.com>
2021-07-01 18:15:23 -0300

    Mueve código de funciones.js a index.js
    Mueve el código de funciones.js a index.js para poder acceder a las variables globales en la jugada del usuario.

--- index.js
+++ index.js
@@ -7,22 +7,69 @@ const $botonEmpezar = document.querySelector('#empezar');
 $botonEmpezar.onclick = jugar;
 
 function jugar() {
-    secuenciaUsuario = [];
-    secuenciaComputadora = [];
-    puntaje = 0;
-    let usuarioAcerto;
-    let continuaRonda = true;
+    for (let i = 1; i <= 5; i++) {
+        jugarRonda();
+    }
+}
 
-    while (continuaRonda) {
-        turnoComputadora(secuenciaComputadora);
-        usuarioAcerto = turnoUsuario(secuenciaUsuario, secuenciaComputadora);
-        console.log(usuarioAcerto)
-        if (usuarioAcerto) {
-            puntaje++;
-        }
-        else {
-            continuaRonda = false;
+function jugarRonda() {
+    desactivarInputUsuario();
+    turnoComputadora(secuenciaComputadora);
+
+    console.log(activarInputUsuario());
+}
+
+function seleccionarBotonAleatorio(listaBotones) {
+    const indice = Math.floor(Math.random() * (4 - 0));
+    return listaBotones[indice];
+}
+
+function resaltarElemento(elemento) {
+    elemento.style.opacity = 1.0;
+    setTimeout(function(){elemento.style.opacity = 0.5}, 800);
+}
+
+function resaltarElementoClickeado(elemento) {
+    elemento.style.opacity = 1.0;
+    setTimeout(function(){elemento.style.opacity = 0.5}, 200);
+}
+
+function compararJugadas(secuenciaComputadora, secuenciaUsuario) {
+    for(let i = 0; i < secuenciaComputadora.length; i++) {
+        if (secuenciaComputadora[i] !== secuenciaUsuario[i]) {
+            return false;
         }
     }
+    return true;
+}
 
-    }
+function turnoComputadora(secuenciaComputadora) {
+    const $circulos = document.querySelectorAll('.circulo');
+    let elementoBoton = seleccionarBotonAleatorio($circulos);
+    secuenciaComputadora.push(Number(elementoBoton.innerText) - 1);
+        
+    secuenciaComputadora.forEach((indiceElemento, contador) => {
+        let elementoAResaltar = $circulos[indiceElemento];
+        setTimeout(function(){resaltarElemento(elementoAResaltar)}, contador * 1000);
+    });
+}
+
+function turnoUsuario(e) {
+    let $circulo = e.target;
+    console.log($circulo.innerText);
+
+    resaltarElementoClickeado($circulo);
+    secuenciaUsuario.push(Number(circulo.innerText -  1));
+    
+    return compararJugadas(secuenciaComputadora, secuenciaUsuario);
+}
+
+function activarInputUsuario(){
+    const $circulos = document.querySelectorAll('.circulo');
+    $circulos.forEach(circulo => {circulo.onclick = turnoUsuario})
+}
+
+function desactivarInputUsuario() {
+    const $circulos = document.querySelectorAll('.circulo');
+    $circulos.forEach(circulo => {circulo.onclick = function(){console.log('Input bloqueado!')}})
+}
